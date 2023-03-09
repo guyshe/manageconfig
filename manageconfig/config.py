738cd81d7c464703167d7df0766fca4a59e35f2b
guy shemesh <theguyshemesh@gmail.com>
2023-03-10 00:33:55 +0200

    Changed library name

--- manageconfig/config.py
+++ manageconfig/config.py
@@ -3,14 +3,14 @@ from typing import Dict
 from pathlib import Path
 
 
-class YamlConfig:
+class Config:
     def __init__(self, config_items: Dict) -> None:
         for key, value in config_items.items():
-            attr_value = YamlConfig(value) if isinstance(value, dict) else value
+            attr_value = Config(value) if isinstance(value, dict) else value
             self.__setattr__(key, attr_value)
     
     @staticmethod
-    def load_from_file(path: Path):
+    def load_from_yml(path: Path):
         with open(path) as f:
-            return YamlConfig(yaml.load(f, Loader=yaml.FullLoader))
+            return Config(yaml.load(f, Loader=yaml.FullLoader))
 
